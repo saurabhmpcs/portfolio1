3dcfd1b744a7a662a943dcb9381a48e398bd09e8
Saurabh Kumar Singh <83777318+saurabhmpcs@users.noreply.github.com>
2024-04-05 14:51:17 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -66,10 +66,10 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Saurabh.</span> I'm a{" "}
-        <span className="font-bold">Frontend developer</span> with{" "}
+        <span className="font-bold">Backend developer</span> with{" "}
         <span className="font-bold">1 year</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">Mern Stack</span>.
       </motion.h1>
 
       <motion.div
